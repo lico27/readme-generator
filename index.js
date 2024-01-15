42817706c8126b03f489eab9625c3479c4541f45
Liam Cottrell <lemcottrell@gmail.com>
2024-01-15 19:57:14 +0000

    make init function

--- index.js
+++ index.js
@@ -5,17 +5,40 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your name?',
+    }
 ];
-
-// function to write README file
-function writeToFile(fileName, data) {
-}
 
 // function to initialize program
 function init() {
-    console.log("test");
+    inquirer
+    .prompt(questions)
+    .then((data) => {
+        const readmeTitle = `${data.title.toLowerCase().split(' ').join('')}.md`;
+    
+        fs.writeFile(readmeTitle, JSON.stringify(data, null, '\t'), (err) =>
+          err ? console.log(err) : console.log('Success!')
+        );
+      });
 }
 
-// function call to initialize program
+
+// // function to initialize program (immediately called when file runs)
+// function init() {
+//     // where we can prompt the user via inquirer.prompt() and then get the answers back in an object with properties for each response 
+//     // e.g. inquirer.prompt(questions).then(answers => do stuff with them)
+//     // we can pass the answers data to our generateMarkdown function to get back the string of text for the readme with all the user's answers filled into it
+//     // e.g generateMarkdown(answers) -> returns some text for the readme
+//     // then we can call the writeToFile function with the file name and the data from the answers object 
+//     // e.g. writeToFile('blabla.md', answers)
+// }
+
+
+
+
+// // function call to initialize program
 init();
+// generateMarkdown(123);
