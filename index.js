ecd8826521f844d8f883678d00e903f582141344
Liam Cottrell <lemcottrell@gmail.com>
2024-01-15 21:42:46 +0000

    add questions

--- index.js
+++ index.js
@@ -1,15 +1,59 @@
 const fs = require("fs");
 const path = require('path');
 const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
     {
         type: 'input',
         name: 'title',
-        message: 'What is your name?',
-    }
+        message: 'Project title',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Description of project',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Installation instructions',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How to use the project',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'How to use the project',
+        choices: [
+            'MIT',
+            'GNU',
+            'Apache',
+            'BSD',
+            'Mozilla',
+            'Eclipse',
+            'Creative Commons',
+            'Unlicense',
+        ]
+    },
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'Credits',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Tests for your project',
+    },
+    {
+        type: 'input',
+        name: 'questions',
+        message: 'Contact details for questions',
+    },
 ];
 
 // function to initialize program
@@ -18,27 +62,11 @@ function init() {
     .prompt(questions)
     .then((data) => {
         const readmeTitle = `${data.title.toLowerCase().split(' ').join('')}.md`;
-    
         fs.writeFile(readmeTitle, JSON.stringify(data, null, '\t'), (err) =>
           err ? console.log(err) : console.log('Success!')
         );
       });
-}
-
-
-// // function to initialize program (immediately called when file runs)
-// function init() {
-//     // where we can prompt the user via inquirer.prompt() and then get the answers back in an object with properties for each response 
-//     // e.g. inquirer.prompt(questions).then(answers => do stuff with them)
-//     // we can pass the answers data to our generateMarkdown function to get back the string of text for the readme with all the user's answers filled into it
-//     // e.g generateMarkdown(answers) -> returns some text for the readme
-//     // then we can call the writeToFile function with the file name and the data from the answers object 
-//     // e.g. writeToFile('blabla.md', answers)
-// }
-
-
-
+};
 
 // // function call to initialize program
 init();
-// generateMarkdown(123);
--- utils/generateMarkdown.js
+++
@@ -1,9 +0,0 @@
-// function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data}
-  
-
-`;
-};
-
-module.exports = generateMarkdown;
